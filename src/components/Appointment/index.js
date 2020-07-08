d07a4bbee842c2d0f34043dc87b4f72e51f578a8
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-07-08 13:12:50 -0400

    added story for booked and empty appointments. updated component to show empty or booked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {Fragment} from "react";
+
+import Header from "components/Appointment/Header";
+
+import Show from "components/Appointment/Show";
+
+import Empty from "components/Appointment/Empty";
 
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      <header>{props.time}</header>
+      {props.interview ? (
+        <Show student={props.interview.student} interviewer={props.interview.interviewer.name} />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
