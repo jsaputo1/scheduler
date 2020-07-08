3567176fa5c372c85e906c239714969582099e06
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-07-07 22:09:11 -0400

    added code for Empty and Header components. Added to story

--- stories/index.js
+++ stories/index.js
@@ -6,16 +6,15 @@ import {action} from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
-
 import DayListItem from "components/DayListItem";
-
 import DayList from "components/DayList";
-
 import InterviewerListItem from "components/InterviewerListItem";
-
 import InterviewerList from "components/InterviewerList";
 
+//Appointment
 import Appointment from "components/Appointment/";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -115,4 +114,6 @@ storiesOf("Appointment", module)
     backgrounds: [{name: "white", value: "#fff", default: true}],
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />);
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
