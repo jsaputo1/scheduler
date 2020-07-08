71a6887ef856bb116570224c12d9bea179536c55
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-07-08 14:44:26 -0400

    Added appointments to application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,8 @@ import "components/Application.scss";
 
 import DayList from "components/DayList";
 
+import Appointment from "components/Appointment";
+
 const days = [
   {
     id: 1,
@@ -22,8 +24,49 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "John Saputo",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcom",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      },
+    },
+  },
+  {
+    id: "last",
+    time: "5pm",
+  },
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const appointmentList = appointments.map((appointment) => <Appointment key={appointment.id} {...appointment} />);
+  console.log(appointmentList);
 
   return (
     <main className="layout">
@@ -35,9 +78,11 @@ export default function Application(props) {
         </nav>
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
+      <section className="schedule">{appointmentList}</section>
     </main>
   );
 }
+
+// const parsedCities = cities.map(city => <h3 onClick={() => setCurrentCity(city)}>{city ? city : "Reset"}</h3>)
+
+// <section className="schedule">{appointments.map((appointment) => appointment.id)}</section>
