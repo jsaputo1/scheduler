e8cbed37b4c0de6caecd3627d2b947b07d649d07
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-07-08 17:05:25 -0400

    added axios as a dependency

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,9 @@
-import React, {useState} from "react";
-
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 import "components/Application.scss";
 
-import DayList from "components/DayList";
-
-import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -65,8 +46,13 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days").then((response) => setDays(response.data));
+  }, []);
+
   const appointmentList = appointments.map((appointment) => <Appointment key={appointment.id} {...appointment} />);
-  console.log(appointmentList);
 
   return (
     <main className="layout">
