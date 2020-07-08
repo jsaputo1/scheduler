c13557cf818f4ebf8edbf6dfc07363930732b09d
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-07-07 22:01:39 -0400

    created Form page, added template code

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+
+import "components/Appointment/styles.scss";
+
+import InterviewerList from "components/InterviewerList";
+
+import Button from "components/Button";
+
+import classNames from "classnames";
+
+export default function Form(props) {
+  // const buttonClass = classNames("form", {});
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            value="name"
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+          This must be a controlled component
+        */
+          />
+        </form>
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger>Cancel</Button>
+          <Button confirm>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
