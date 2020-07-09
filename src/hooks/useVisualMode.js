1dacae222fb89c184a90c2f387f90ec1042f7132
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-07-09 16:19:07 -0400

    created visual mode custom hook and added tests

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(mode, replace = false) {
+    if (replace) {
+      setMode(mode);
+      history[history.length - 1] = mode;
+    } else {
+      setMode(mode);
+      setHistory(prev => ([...prev, mode]));
+    }
+  }
+
+  function back() {
+    if (history.length === 1) return;
+    history.pop();
+    setMode(history[history.length - 1]);
+  }
+  return { mode, transition, back };
+}
