09e81e8c25802e9571538a19374433e51595100c
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-07-08 20:59:12 -0400

    wrote helper function to get appointements for a day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,10 +2,8 @@ export function getAppointmentsForDay(state, day) {
   const filteredDay = state.days.filter((stateDay) => stateDay.name === day);
   const appointmentArray = [];
   if (filteredDay && filteredDay.length > 0) {
-    for (let appointment in state.appointments) {
-      if (filteredDay[0].appointments.includes(Number(appointment))) {
-        appointmentArray.push(state.appointments[appointment]);
-      }
+    for (let appointmentNumber of filteredDay[0].appointments) {
+      appointmentArray.push(state.appointments[appointmentNumber]);
     }
     return appointmentArray;
   } else {
