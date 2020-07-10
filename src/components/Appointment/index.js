c8929c6248b301ccb57edf0e1f52dd7d66d5f7a2
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-07-09 23:25:58 -0400

    added custom visual mode hook. added empty, show and create modes using the hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, { Fragment, useEffect } from "react";
 
 import Header from "components/Appointment/Header";
 
@@ -6,16 +6,42 @@ import Show from "components/Appointment/Show";
 
 import Empty from "components/Appointment/Empty";
 
+import useVisualMode from "hooks/useVisualMode";
+
 import "components/Appointment/styles.scss";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  )
+  // useEffect(() => {
+  //   if (props.interview && mode == EMPTY) {
+  //     transition(SHOW)
+  //   }
+
+  // }, [props.interview]);
+
   return (
     <article className="appointment">
       <header>{props.time}</header>
-      {props.interview ? (
-        <Show student={props.interview.student} interviewer={props.interview.interviewer.name} />
-      ) : (
-        <Empty />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => back()}
+          onSave={() => back()}
+        />
       )}
     </article>
   );
