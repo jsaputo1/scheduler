b3cec44c63b6f211fecf7b4dba21588feafaea5d
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-07-15 15:41:55 -0400

    added conditional to check if the interviewer is selected

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -14,15 +14,17 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (interviewer === null) {
+      setError("Interviewer has to be selected");
+      return;
+    }
     setError("");
     props.onSave(name, interviewer);
   }
-
   const reset = () => {
     setName("");
     setInterviewer(null);
   };
-
   const cancel = () => {
     reset();
     props.onCancel();
